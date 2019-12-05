c7a379cf0aa9d492ba1e9fd77fe38c48ad86bcd1
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-12-05 17:35:56 +0700

    Update script updater

---
+++ script_updater/README.md
@@ -0,0 +1,10 @@
+# elrond-tools / script_updater
+
+Simple utility to allow for easier upgrades of elrond-go-scripts-v2
+
+## Installation
+
+```
+curl -LOs https://raw.githubusercontent.com/SebastianJ/elrond-tools/master/script_updater/script_updater.sh && chmod +x script_updater.sh
+./status.sh
+```
--- script_updater/script_updater.sh
+++ script_updater/script_updater.sh
@@ -1,23 +1,75 @@
 #!/bin/bash
 
 # Elrond script updater
-script_repo="$HOME/elrond/script/elrond-go-scripts-v2"
-destination_folder="$HOME/elrond/script"
 
-echo "Updating elrond-go-scripts-v2 in $script_repo"
+usage() {
+   cat << EOT
+Usage: $0 [option] command
+Options:
+   --repo     path    the output path for compiled binaries
+   --path     path    the go path where git repositories should be cloned, will default to $GOPATH
+   --help             print this help section
+EOT
+}
 
-cd $script_repo
-git fetch
-git checkout --force master
-git pull
+while [ $# -gt 0 ]
+do
+  case $1 in
+  --repo) repo_path="${2%/}" ; shift;;
+  --path) destination_path="${2%/}" ; shift;;
+  -h|--help) usage; exit 1;;
+  (--) shift; break;;
+  (-*) usage; exit 1;;
+  (*) break;;
+  esac
+  shift
+done
 
-echo "Updated git repo (fetch/checkout/pull)"
+if [ -z "$repo_path" ]; then
+  repo_path="$HOME/elrond/script/elrond-go-scripts-v2"
+fi
 
-echo "Copying new release to $destination_folder"
+if [ -z "$destination_path" ]; then
+  destination_path="$HOME/elrond/script"
+fi
 
-rm -rf $destination_folder/auto-updater.sh $destination_folder/config $destination_folder/script.sh
-cp -R $script_repo/auto-updater.sh $script_repo/script.sh $script_repo/config .
-rm -rf $destination_folder/config/variables.cfg
-cp $destination_folder/variables.cfg $destination_folder/config/
+update_repo() {
+  echo "Updating elrond-go-scripts-v2 in $repo_path"
 
-echo "Successfully updated elrond-go-scripts-v2!"
+  if test -d $repo_path; then
+    cd $repo_path
+    git fetch >/dev/null 2>&1
+    checkout_message=$(git checkout --force master)
+    already_updated_message="Your branch is up to date"
+
+    if [[ "$checkout_message" =~ "$already_updated_message" ]]; then
+      echo "You already have the latest version of elrond-go-scripts-v2!"
+    else
+      git pull
+
+      install_files
+    fi
+  else
+    mkdir -p $repo_path
+    cd $repo_path
+    git clone https://github.com/ElrondNetwork/elrond-go-scripts-v2.git
+  fi
+}
+
+install_files() {
+  echo "Copying new release to $destination_path"
+
+  rm -rf $destination_path/auto-updater.sh $destination_path/config $destination_path/script.sh
+  cp -R $repo_path/auto-updater.sh $repo_path/script.sh $repo_path/config $destination_path
+
+  if test -f $destination_path/variables.cfg; then
+    rm -rf $destination_path/config/variables.cfg
+    cp $destination_path/variables.cfg $destination_path/config/
+  fi
+
+  echo "Successfully updated elrond-go-scripts-v2!"
+}
+
+
+
+
